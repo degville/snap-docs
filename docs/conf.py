599fc2adcbc55501ee9c194b32debdb35c0269ac
Graham Morrison <graham.morrison@canonical.com>
2025-10-13 12:39:33 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -175,6 +175,9 @@ html_context = {
 # Sitemap configuration: https://sphinx-sitemap.readthedocs.io/
 #######################
 
+# GM: try to clear sitemap recursion error
+sys.setrecursionlimit(1_500)
+
 # Base URL of RTD hosted project
 
 html_baseurl = 'https://canonical-starter-pack.readthedocs-hosted.com/'
