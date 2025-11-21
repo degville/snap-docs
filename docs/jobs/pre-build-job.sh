05584bd1cbcb8a71e44c61dc71c5b02a274d33dc
Robert Fudge <robert.fudge@canonical.com>
2025-11-21 19:11:02 -0330

    snap-docs: make ${BUILDDIR}/reference/ folder

--- docs/jobs/pre-build-job.sh
+++ docs/jobs/pre-build-job.sh
@@ -8,5 +8,6 @@ echo "Downloading OpenAPI docs from canonical/snapd-rest-openapi..."
 LATEST_RUN_ID=$(gh run list -R canonical/snapd-rest-openapi --workflow redocly.yaml --limit 1 --json databaseId -q '.[0].databaseId')
 echo "Found Run ID: $LATEST_RUN_ID"
 gh run download "$LATEST_RUN_ID" -R canonical/snapd-rest-openapi -n api-docs -D "${SOURCEDIR}/reference/api"
+mkdir -p ${BUILDDIR}/reference/
 mv ${SOURCEDIR}/reference/api/ ${BUILDDIR}/reference/
 echo "OpenAPI docs downloaded to ${BUILDDIR}/reference/api"
