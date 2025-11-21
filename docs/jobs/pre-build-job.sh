edfc88161c27d4511e69583b3f685bfc41484945
Robert Fudge <rnfudge@mun.ca>
2025-11-21 18:40:42 -0330

    snap-docs: integrate pre-build and post-build steps

--- docs/jobs/pre-build-job.sh
+++ docs/jobs/pre-build-job.sh
@@ -8,5 +8,5 @@ echo "Downloading OpenAPI docs from canonical/snapd-rest-openapi..."
 LATEST_RUN_ID=$(gh run list -R canonical/snapd-rest-openapi --workflow redocly.yaml --limit 1 --json databaseId -q '.[0].databaseId')
 echo "Found Run ID: $LATEST_RUN_ID"
 gh run download "$LATEST_RUN_ID" -R canonical/snapd-rest-openapi -n api-docs -D "${SOURCEDIR}/reference/api"
-
-echo "OpenAPI docs downloaded to ${SOURCEDIR}/reference/api"
+mv ${SOURCEDIR}/reference/api/ ${BUILDDIR}/reference/
+echo "OpenAPI docs downloaded to ${BUILDDIR}/reference/api"
