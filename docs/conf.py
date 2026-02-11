4a371fc3235b640ff3b8fef0451c57b8eb30786f
secondskoll <michael.park@canonical.com>
2026-02-11 10:47:43 +1000

    fix: ogp site url

--- docs/conf.py
+++ docs/conf.py
@@ -70,7 +70,7 @@ copyright = "%s CC-BY-SA, %s" % (datetime.date.today().year, author)
 # NOTE: The Open Graph Protocol (OGP) enhances page display in a social graph
 #       and is used by social media platforms; see https://ogp.me/
 
-ogp_site_url = "https://canonical-snap.readthedocs-hosted.com/"
+ogp_site_url = "https://snapcraft.io/docs/"
 
 
 # Preview name of the documentation website
